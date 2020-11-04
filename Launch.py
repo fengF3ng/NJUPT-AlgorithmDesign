758a80e1e18d60894ed4da20566ec5ee4346fea9
redgrape01 <1711623223@qq.com>
2020-11-04 15:18:36 +0800

    'second'

--- Launch.py
+++ Launch.py
@@ -1,6 +1,9 @@
 import ctypes
 import sys
+from random import *
 
+import win32con
+import win32ui
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
@@ -21,115 +24,190 @@ print(s)
 print(QBlibc.py_calculate(bytes(s,encoding='utf-8')))
 '''
 
-class QBWindows(QMainWindow,Ui_QuestionBankUI.Ui_MainWindow):
 
-    def __init__(self,parent = None):
+class QBWindows(QMainWindow, Ui_QuestionBankUI.Ui_MainWindow):
+
+    def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+
         self.blank = 10
         self.choice = 0
         self.judge = 0
+        self.bool_decimal = 0
+        self.bool_negative = 0
+        self.int_len = 10
 
         self.actionSave.triggered.connect(self.QB_save)
         self.actionExit.triggered.connect(QCoreApplication.instance().quit)
+
         self.negative.stateChanged.connect(self.set_negative)
         self.decimal.stateChanged.connect(self.set_decimal)
         self.bracket.stateChanged.connect(self.set_bracket)
         self.remain.currentIndexChanged.connect(self.set_remain)
         self.len.currentIndexChanged.connect(self.set_len)
+
         self.blank_num.textChanged.connect(self.blank_num_balance)
         self.choice_num.textChanged.connect(self.choice_num_balance)
         self.judge_num.textChanged.connect(self.judge_num_balance)
+
         self.get_questions.clicked.connect(self.QB_get_questions)
         self.judge_questions.clicked.connect(self.QB_judge_questions)
         self.clear_questions.clicked.connect(self.clear_all)
 
-        self.problems = []
-        self.ans = []
-        self.user_ans = []
+        self.problems = []  # 题面组件
+        self.ans = []  # 答题框
+        self.user_ans = []  # 答题组件
+        self.standard_ans = []  # 标准答案
+        self.user_problems = ""  # 题面
+        self.user_analysis = ""  # 分析
+
         for i in range(10):
-            self.problems.append(QLabel(self.gridLayoutWidget_2)) 
-            self.gridLayout_2.addWidget(self.problems[i],i+1, 1, 1, 1)
+            self.problems.append(QLabel(self.gridLayoutWidget_2))
+            self.gridLayout_2.addWidget(self.problems[i], i+1, 1, 1, 1)
         for i in range(10):
             self.ans.append(QHBoxLayout(self.gridLayoutWidget_2))
-            self.gridLayout_2.addLayout(self.ans[i],i+1, 2, 1, 1)
+            self.gridLayout_2.addLayout(self.ans[i], i+1, 2, 1, 1)
+
     def QB_save(self):
-        print("save")
-    def set_negative(self,state):
-        if state==Qt.Checked:
+        dlg = win32ui.CreateFileDialog(
+            0, None, None, win32con.OFN_OVERWRITEPROMPT | win32con.OFN_FILEMUSTEXIST, "TXT Files(*.txt)")
+        dlg.SetOFNInitialDir("C:")
+        if dlg.DoModal() == win32con.IDOK:
+            f = open(dlg.GetPathName()+'.txt', 'w')
+            f.write(self.user_problems)
+            f.write("Aanalysis:\n")
+            f.write(self.user_analysis)
+            f.close()
+
+    def set_negative(self, state):
+        if state == Qt.Checked:
             QBlibc.py_set_negative(1)
+            self.bool_negative = 1
         else:
             QBlibc.py_set_negative(0)
-    def set_decimal(self,state):
-        if state==Qt.Checked:
+            self.bool_negative = 0
+
+    def set_decimal(self, state):
+        if state == Qt.Checked:
             QBlibc.py_set_decimal(1)
+            self.bool_decimal = 1
         else:
             QBlibc.py_set_decimal(0)
-    def set_bracket(self,state):
-        if state==Qt.Checked:
+            self.bool_decimal = 0
+
+    def set_bracket(self, state):
+        if state == Qt.Checked:
             QBlibc.py_set_bracket(1)
         else:
             QBlibc.py_set_bracket(0)
-    def set_remain(self,state):
+
+    def set_remain(self, state):
         QBlibc.py_set_remain(state+2)
-    def set_len(self,state):
+
+    def set_len(self, state):
         QBlibc.py_set_len(10**(state+1))
-    def blank_num_balance(self,state):
+        self.int_len = 10**(state+1)
+
+    def blank_num_balance(self, state):
         try:
             state = int(state)
         except:
-            print("error")
+            pass
         else:
-            if state+self.choice+self.judge>10:
-                self.blank_num.setText(_translate("MainWindow",str(10-self.choice-self.judge)))
+            if state+self.choice+self.judge > 10:
+                self.blank_num.setText(_translate(
+                    "MainWindow", str(10-self.choice-self.judge)))
                 self.blank = 10-self.choice-self.judge
             else:
                 self.blank = state
-    def choice_num_balance(self,state):
+
+    def choice_num_balance(self, state):
         try:
             state = int(state)
         except:
-            print("error")
+            pass
         else:
-            if state+self.blank+self.judge>10:
-                self.choice_num.setText(_translate("MainWindow",str(10-self.blank-self.judge)))
+            if state+self.blank+self.judge > 10:
+                self.choice_num.setText(_translate(
+                    "MainWindow", str(10-self.blank-self.judge)))
                 self.choice = 10-self.blank-self.judge
             else:
                 self.choice = state
-    def judge_num_balance(self,state):
+
+    def judge_num_balance(self, state):
         try:
             state = int(state)
         except:
-            print("error")
+            pass
         else:
-            if state+self.choice+self.judge>10:
-                self.judge_num.setText(_translate("MainWindow",str(10-self.choice-self.blank)))
+            if state+self.choice+self.judge > 10:
+                self.judge_num.setText(_translate(
+                    "MainWindow", str(10-self.choice-self.blank)))
                 self.judge = 10-self.choice-self.blank
             else:
                 self.judge = state
+
     def QB_get_questions(self):
         self.clear_all()
-        for i in range(1,self.blank+1):
+        for i in range(1, self.blank+1):
             self.add_blank(i)
-        for i in range(self.blank+1,self.blank+self.choice+1):
+        for i in range(self.blank+1, self.blank+self.choice+1):
             self.add_choice(i)
-        for i in range(self.blank+self.choice+1,self.blank+self.choice+self.judge+1):
+        for i in range(self.blank+self.choice+1, self.blank+self.choice+self.judge+1):
             self.add_judge(i)
-    def add_blank(self,index):
+
+    def add_blank(self, index):
         s = QBlibc.py_generate().decode()
-        QBlibc.py_set_remain(self.remain.currentIndex()+2)
-        self.problems[index-1].setText(_translate("MainWindow",s))
+        if self.bool_decimal:
+            self.standard_ans.append(
+                float('%.1f' % QBlibc.py_calculate(bytes(s, encoding='utf-8'))))
+        else:
+            self.standard_ans.append(
+                int(QBlibc.py_calculate(bytes(s, encoding='utf-8'))))
+        self.problems[index-1].setText(_translate("MainWindow", s))
+        self.user_problems += s + '\n' + 'Correct answer:' + \
+            str(self.standard_ans[index-1]) + '\n'
 
         wid = []
         wid.append(QLineEdit())
         self.ans[index-1].addWidget(wid[0])
         self.user_ans.append(wid)
-    def add_choice(self,index):
-        s = QBlibc.py_generate().decode()
-        QBlibc.py_set_remain(self.remain.currentIndex()+2)
-        self.problems[index-1].setText(_translate("MainWindow",s))
 
-        buttonGroup=QButtonGroup(self.gridLayoutWidget_2)
+    def add_choice(self, index):
+        s = QBlibc.py_generate().decode()
+        ans_part = []
+        if self.bool_decimal:
+            tmp = QBlibc.py_calculate(bytes(s, encoding='utf-8'))
+            ans_part.append(float('%.1f' % tmp))
+        else:
+            ans_part.append(
+                int(QBlibc.py_calculate(bytes(s, encoding='utf-8'))))
+        s += '\n'
+        right_ans = randint(0, 3) % 4
+        for i in range(4):
+            if right_ans == i:
+                ans_part.append(i)
+                s += chr(ord('A')+i) + "." + str(ans_part[0]) + "  "
+            else:
+                s += chr(ord('A')+i) + "."
+                # while true:
+                if self.bool_negative and self.bool_decimal:
+                    tmp = uniform(-self.int_len, self.int_len)
+                    s += str(float('%.1f' % tmp)) + "  "
+                elif self.bool_negative and not self.bool_decimal:
+                    s += str(randint(-self.int_len, self.int_len)) + "  "
+                elif not self.bool_negative and self.bool_decimal:
+                    tmp = uniform(0, self.int_len)
+                    s += str(float('%.1f' % tmp)) + "  "
+                elif not self.bool_negative and not self.bool_decimal:
+                    s += str(randint(0, self.int_len)) + "  "
+        self.standard_ans.append(ans_part)
+        self.problems[index-1].setText(_translate("MainWindow", s))
+        self.user_problems += s + '\n' + 'Correct answer:' + \
+            chr(ord('A')+right_ans) + '\n'
+
+        buttonGroup = QButtonGroup(self.gridLayoutWidget_2)
         wid = []
         wid.append(QRadioButton())
         wid[0].setText(_translate("MainWindow", "A"))
@@ -149,12 +227,43 @@ class QBWindows(QMainWindow,Ui_QuestionBankUI.Ui_MainWindow):
         self.ans[index-1].addWidget(wid[2])
         self.ans[index-1].addWidget(wid[3])
         self.user_ans.append(wid)
-    def add_judge(self,index):
-        s = QBlibc.py_generate().decode()
-        QBlibc.py_set_remain(self.remain.currentIndex()+2)
-        self.problems[index-1].setText(_translate("MainWindow",s))
 
-        buttonGroup=QButtonGroup(self.gridLayoutWidget_2)
+    def add_judge(self, index):
+        s = QBlibc.py_generate().decode()
+        ans_part = []
+        if self.bool_decimal:
+            tmp = QBlibc.py_calculate(bytes(s, encoding='utf-8'))
+            ans_part.append(float('%.1f' % tmp))
+        else:
+            ans_part.append(
+                int(QBlibc.py_calculate(bytes(s, encoding='utf-8'))))
+        right_ans = randint(0, 1) % 4
+        if right_ans:
+            ans_part.append(1)
+            s += '=' + str(ans_part[0])
+        else:
+            ans_part.append(0)
+            s += '='
+            wrong_ans = ''
+            while True:
+                if self.bool_negative and self.bool_decimal:
+                    tmp = uniform(-self.int_len, self.int_len)
+                    wrong_ans = str(float('%.1f' % tmp))
+                elif self.bool_negative and not self.bool_decimal:
+                    wrong_ans = str(randint(-self.int_len, self.int_len))
+                elif not self.bool_negative and self.bool_decimal:
+                    tmp = uniform(0, self.int_len)
+                    wrong_ans = str(float('%.1f' % tmp))
+                elif not self.bool_negative and not self.bool_decimal:
+                    wrong_ans = str(randint(0, self.int_len))
+                if wrong_ans != ans_part[0]:
+                    break
+            s += wrong_ans
+        self.standard_ans.append(ans_part)
+        self.problems[index-1].setText(_translate("MainWindow", s))
+        self.user_problems += s + '\n' + ('√' if right_ans else '×') + '\n'
+
+        buttonGroup = QButtonGroup(self.gridLayoutWidget_2)
         wid = []
         wid.append(QRadioButton())
         wid[0].setText(_translate("MainWindow", "√"))
@@ -166,33 +275,64 @@ class QBWindows(QMainWindow,Ui_QuestionBankUI.Ui_MainWindow):
         self.ans[index-1].addWidget(wid[0])
         self.ans[index-1].addWidget(wid[1])
         self.user_ans.append(wid)
+
     def QB_judge_questions(self):
-        user_analysis = ""
+        self.user_analysis = ""
         score = 0
         j = 0
         for i in self.user_ans:
-            if len(i)==4: # choice
-                pass
-            elif len(i)==2: # judge
-                pass
-            elif len(i)==1: # blank
-                if abs(QBlibc.py_calculate(bytes(self.problems[j],encoding='utf-8'))-i)<0.1:
+            if len(i) == 4:  # choice
+                if i[self.standard_ans[j][1]].isChecked():
                     score += 1
+                else:
+                    self.user_analysis += 'Wrong in ' + \
+                        str(j+1) + ",the corret answer is " + \
+                        chr(ord('A')+self.standard_ans[j][1]) + "\n"
+            elif len(i) == 2:  # judge
+                if i[self.standard_ans[j][1]].isChecked():
+                    self.user_analysis += 'Wrong in ' + \
+                        str(j+1) + ",the corret answer is " + \
+                        ('√' if self.standard_ans[j][1] else '×') + "\n"
+                else:
+                    score += 1
+            elif len(i) == 1:  # blank
+                if self.bool_decimal:
+                    if float(i[0].text())-self.standard_ans[j] < 0.1:
+                        score += 1
+                    else:
+                        self.user_analysis += 'Wrong in ' + \
+                            str(j+1) + ",the corret answer is " + \
+                            str(self.standard_ans[j]) + "\n"
+                else:
+                    if int(i[0].text()) == self.standard_ans[j]:
+                        score += 1
+                    else:
+                        self.user_analysis += 'Wrong in ' + \
+                            str(j+1) + ",the corret answer is " + \
+                            str(self.standard_ans[j]) + "\n"
             j += 1
-        self.analysis.setText(_translate("MainWindow",user_analysis))
+        self.user_analysis += 'Your get ' + str(score)
+        self.analysis.setText(_translate("MainWindow", self.user_analysis))
 
     def clear_all(self):
         self.user_ans.clear()
+        self.user_problems = ""
+        self.user_analysis = ""
+        self.standard_ans.clear()
         for i in self.ans:
             while i.count():
                 i.takeAt(0).widget().deleteLater()
         for i in self.problems:
-            i.setText(_translate("MainWindow",""))
+            i.setText(_translate("MainWindow", ""))
+        self.analysis.setText(_translate("MainWindow", ""))
 
-        
 
 if __name__ == "__main__":
-    app=QApplication(sys.argv)
+    app = QApplication(sys.argv)
     QBui = QBWindows()
-    QBui.show()
-    sys.exit(app.exec())
+    try:
+        QBui.show()
+    except:
+        pass
+    finally:
+        sys.exit(app.exec())
--- QuestionBank.cpp
+++ QuestionBank.cpp
@@ -1,292 +1,319 @@
-#include<iostream>
-#include<vector>
-#include<string.h>
-#include<stack>
-#include<stdlib.h>
-#include<time.h>
+#include <iostream>
+#include <stack>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <vector>
 using namespace std;
 
+class QuestionBank {
+  protected:
+    /**
+     * var
+     **/
+    static const int maxn = 1e2;     // length of expression
+    static const int questions = 10; // nums of questions
+    int remain, len;
+    bool negative, decimal, bracket;
+    /**
+     * method
+     **/
+    int icp(char a); // in-stack priority
+    int isp(char a);
+    void change(string,
+                char[maxn][maxn]); // infix expression to suffix expression
+    string generate(unsigned int seed = rand(), int flag = 0);
 
-class QuestionBank{
-	protected:
-		/**
-		 * var
-		 **/
-		static const int maxn = 1e2;//length of expression
-		static const int questions = 10; // nums of questions
-		int remain,len;
-		bool negative,decimal,bracket;
-		/**
-		 * method
-		 **/
-		int icp(char a); // in-stack priority
-		int isp(char a);
-		void change(string,char[maxn][maxn]); // infix expression to suffix expression
-	public:
-		QuestionBank();
-		~QuestionBank();
-		/**
-		 * set attr
-		 **/
-		void set_remain(int);
-		void set_len(int);
-		void set_negative(bool);
-		void set_decimal(bool);
-		void set_bracket(bool);
-		/**
-		 * main public method
-		 **/
-		string generate(unsigned int seed = rand(),int flag = 0); 
-		double calculate(string); // calculate suffix expression	
-}; 
+  public:
+    QuestionBank();
+    ~QuestionBank();
+    /**
+     * set attr
+     **/
+    void set_remain(int);
+    void set_len(int);
+    void set_negative(bool);
+    void set_decimal(bool);
+    void set_bracket(bool);
+    /**
+     * main public method
+     **/
+    string get_question();
+    double calculate(string); // calculate suffix expression
+};
 
-extern "C"{
-	/**
-	 * c api for py
-	 **/
-	QuestionBank a;
-	
+extern "C" {
+/**
+ * c api for py
+ **/
+QuestionBank a;
 
-	char* py_test(char *s){
-		return s;
-	}
+char *py_test(char *s) { return s; }
 
-	void py_set_remain(int x){
-		a.set_remain(x);
-	}
+void py_set_remain(int x) { a.set_remain(x); }
 
-	void py_set_len(int x){
-		a.set_len(x);
-	}
+void py_set_len(int x) { a.set_len(x); }
 
-	void py_set_negative(int x){
-		if(x)a.set_negative(true);
-		else a.set_negative(false);
-	}
+void py_set_negative(int x) {
+    if (x)
+        a.set_negative(true);
+    else
+        a.set_negative(false);
+}
 
-	void py_set_decimal(int x){
-		if(x)a.set_decimal(true);
-		else a.set_decimal(false);
-	}
+void py_set_decimal(int x) {
+    if (x)
+        a.set_decimal(true);
+    else
+        a.set_decimal(false);
+}
 
-	void py_set_bracket(int x){
-		if(x)a.set_bracket(true);
-		else a.set_bracket(false);
-	}
+void py_set_bracket(int x) {
+    if (x)
+        a.set_bracket(true);
+    else
+        a.set_bracket(false);
+}
 
-	const char* py_generate(){
-		string s = a.generate();
-		char *p = new char[20];
-		for(int i=0;i<s.length();i++)p[i] = s[i];
-		p[s.length()] = '\x00';
-		return p;
+const char *py_generate() {
+    string s = a.get_question();
+    char *p = new char[20];
+    for (int i = 0; i < s.length(); i++)
+        p[i] = s[i];
+    p[s.length()] = '\x00';
+    return p;
+}
+
+double py_calculate(char *s) { return a.calculate(s); }
+}
+
+void test() {
+    /**
+     * debug mode!!
+     **/
+    QuestionBank b;
+    cout << b.calculate("5-3") << endl;
+}
+
+int main() {
+    cout << "**********************" << endl;
+    cout << "**Designed By HeFeng**" << endl;
+    cout << "**********************" << endl;
+    test();
+    system("pause");
+    return 0;
+}
+
+QuestionBank::QuestionBank()
+    : remain(2), len(10), negative(false), decimal(false), bracket(false) {
+    // constructor
+}
+
+QuestionBank::~QuestionBank() {
+    // destructor
+}
+
+int QuestionBank::icp(char a) {
+    switch (a) {
+    case '#':
+        return 0;
+    case '(':
+        return 12;
+    // case '!':return 10;
+    case '*':
+        return 8;
+    case '/':
+        return 6;
+    case '+':
+        return 4;
+    case '-':
+        return 2;
+    case ')':
+        return 1;
+    default:
+        return -1;
+    }
+}
+
+int QuestionBank::isp(char a) {
+    switch (a) {
+    case '#':
+        return 0;
+    case '(':
+        return 1;
+    // case '!':return 11;
+    case '*':
+        return 9;
+    case '/':
+        return 7;
+    case '+':
+        return 5;
+    case '-':
+        return 3;
+    case ')':
+        return 12;
+    default:
+        return -1;
+    }
+}
+
+void QuestionBank::change(string infix_expression,
+                          char suffix_expression[maxn][maxn]) {
+    int i = 0, j = 0;
+    int ch;
+    char temp;
+    stack<char> s;
+    s.push('#');
+    while (infix_expression[i] != '#') {
+        ch = infix_expression[i++];
+        if ('0' <= ch && ch <= '9') { // operand
+            int k = 1;
+            suffix_expression[j][0] = 'd';
+            while (('0' <= ch && ch <= '9') || ch == '.') {
+                suffix_expression[j][k++] = ch;
+                ch = infix_expression[i++];
+            }
+            suffix_expression[j][k] = '\x00';
+            i--;
+            j++;
+        } else if (ch == ')')
+            for (temp = s.top(), s.pop(); temp != '(';
+                 temp = s.top(), s.pop()) {
+                suffix_expression[j][0] = 'o';
+                suffix_expression[j][1] = temp;
+                j++;
+            }
+        else {
+            for (temp = s.top(), s.pop(); icp(ch) <= isp(temp);
+                 temp = s.top(), s.pop()) {
+                suffix_expression[j][0] = 'o';
+                suffix_expression[j][1] = temp;
+                j++;
+            }
+            s.push(temp);
+            s.push((char)ch);
+        }
+    }
+    while (!s.empty()) {
+        suffix_expression[j][0] = 'o';
+        suffix_expression[j][1] = s.top();
+        j++;
+        s.pop();
+    }
+}
+
+string QuestionBank::generate(unsigned int seed, int flag) {
+    string res;
+    res.clear();
+    srand(seed);
+    remain--;
+    if (flag && bracket)
+        res += '(';
+    if (remain <= 1) {
+        char tmp[10];
+        if (rand() % 2 && decimal) {
+            itoa(rand() % len, tmp, 10);
+            itoa(rand() % 10, tmp + strlen(tmp) + 1, 10);
+            tmp[strlen(tmp)] = '.';
+        } else {
+            itoa(rand() % len, tmp, 10);
+        }
+        res += tmp;
+    } else {
+        res += generate(seed * seed + time(0), rand() % 2);
+    }
+    switch (rand() % 4) {
+    case 0:
+        res += '+';
+        break;
+    case 1:
+        res += '-';
+        break;
+    case 2:
+        res += '*';
+        break;
+    case 3:
+        res += '/';
+    }
+    if (remain <= 1) {
+        char tmp[10];
+        if (rand() % 2 && decimal) {
+            itoa(rand() % len, tmp, 10);
+            itoa(rand() % 10, tmp + strlen(tmp) + 1, 10);
+            tmp[strlen(tmp)] = '.';
+        } else {
+            itoa(rand() % len, tmp, 10);
+        }
+        res += tmp;
+    } else {
+        string tmp = generate(seed * seed * seed + time(0), rand() % 2);
     }
 
-	int py_calculate(char *s){
-		return a.calculate(s);
-	}
+    if (flag && bracket)
+        res += ')';
+    return res;
 }
 
-void test(){
-	/**
-	 * debug mode!!
-	 **/
-	QuestionBank b;
-	cout<<b.calculate("(1.2+1.3)*2.5")<<endl;
-}
+void QuestionBank::set_remain(int x) { remain = x; }
 
-int main(){
-	cout<<"**********************"<<endl;
-	cout<<"**Designed By HeFeng**"<<endl;
-	cout<<"**********************"<<endl;
-	test();
-	system("pause");
-	return 0;
-}
+void QuestionBank::set_len(int x) { len = x; }
 
-QuestionBank::QuestionBank():remain(2),len(10),negative(false),decimal(false),bracket(false){
-	// constructor
-}
+void QuestionBank::set_negative(bool x) { negative = x; }
 
-QuestionBank::~QuestionBank(){
-	// destructor
-}
+void QuestionBank::set_decimal(bool x) { decimal = x; }
 
-int QuestionBank::icp(char a){
-	switch(a){
-		case '#':return 0;
-		case '(':return 12;
-		//case '!':return 10;
-		case '*':return 8;
-		case '/':return 6;
-		case '+':return 4;
-		case '-':return 2;
-		case ')':return 1;
-		default:return -1;
-	}
-}
+void QuestionBank::set_bracket(bool x) { bracket = x; }
 
-int QuestionBank::isp(char a){
-	switch(a){
-		case '#':return 0;
-		case '(':return 1;
-		//case '!':return 11;
-		case '*':return 9;
-		case '/':return 7;
-		case '+':return 5;
-		case '-':return 3;
-		case ')':return 12;
-		default:return -1;
-	}
+string QuestionBank::get_question() {
+    string res;
+    do {
+        int tmp = remain;
+        res = generate();
+        remain = tmp;
+    } while (false);
+    return res;
 }
-
-void QuestionBank::change(string infix_expression,char suffix_expression[maxn][maxn]){
-	int i=0,j=0;
-	int ch;
-	char temp;
-	stack<char>s;
-	s.push('#');
-	while(infix_expression[i]!='#'){
-		ch=infix_expression[i++];
-		if('0'<=ch && ch<='9'){ // operand
-			int k=1;
-			suffix_expression[j][0] = 'd';
-			while(('0'<=ch && ch<='9')||ch=='.'){
-				suffix_expression[j][k++] = ch;
-				ch = infix_expression[i++];
-			}
-			suffix_expression[j][k] = '\x00';
-			i--;
-			j++;
-		}
-		else if(ch==')')
-			for(temp=s.top(),s.pop();temp!='(';temp=s.top(),s.pop()){
-				suffix_expression[j][0] = 'o';
-				suffix_expression[j][1] = temp;
-				j++;
-			}
-		else{
-			for(temp=s.top(),s.pop();icp(ch)<=isp(temp);temp=s.top(),s.pop()){
-				suffix_expression[j][0] = 'o';
-				suffix_expression[j][1] = temp;
-				j++;
-			}
-			s.push(temp);
-			s.push((char)ch);
-		}
-	}
-	while(!s.empty()){
-		suffix_expression[j][0] = 'o';
-		suffix_expression[j][1] = s.top();
-		j++;
-		s.pop();	
-	}
-}
-
-void QuestionBank::set_remain(int x){
-	remain = x;
-}
-
-void QuestionBank::set_len(int x){
-	len = x;
-}
-
-void QuestionBank::set_negative(bool x){
-	negative = x;
-}
-
-void QuestionBank::set_decimal(bool x){
-	decimal = x;
-}
-
-void QuestionBank::set_bracket(bool x){
-	bracket = x;
-}
-
-string QuestionBank::generate(unsigned int seed,int flag){
-	string res;
-	res.clear();
-	srand(seed);
-	remain--;
-	if(flag && bracket)res += '(';
-	if(remain<=1){
-		char tmp[10];
-		if(rand()%2 && decimal){
-			itoa(rand()%len,tmp,10);
-			itoa(rand()%10,tmp+strlen(tmp)+1,10);
-			tmp[strlen(tmp)] = '.';
-		}else{
-			itoa(rand()%len,tmp,10);
-		}
-		res += tmp;
-	}else{
-		res += generate(seed*seed+time(0),rand()%2);
-	}
-	switch(rand()%4){
-		case 0:res += '+';
-				break;
-		case 1:res += '-';
-				break;
-		case 2:res += '*';
-				break;
-		case 3:res += '/';
-	}
-	if(remain<=1){
-		char tmp[10];
-		if(rand()%2 && decimal){
-			itoa(rand()%len,tmp,10);
-			itoa(rand()%10,tmp+strlen(tmp)+1,10);
-			tmp[strlen(tmp)] = '.';
-		}else{
-			itoa(rand()%len,tmp,10);
-		}
-		res += tmp;
-	}else{
-		string tmp = generate(seed*seed*seed+time(0),rand()%2);
-	}
-	
-	if(flag && bracket)res += ')';
-	return res;
-}
-
-double QuestionBank::calculate(string infix_expression){
-	char suffix_expression[maxn][maxn];
-	infix_expression.append(1,'#');
-	change(infix_expression,suffix_expression);
-	stack<double>s;
-	int j=0;
-	while(true){
-		if(suffix_expression[j][0] == 'o' && suffix_expression[j][1]=='#')break;
-		if(suffix_expression[j][0] == 'd'){
-			double tmp = 0;
-			for(int k = 1;suffix_expression[j][k];k++){
-				if(suffix_expression[j][k]=='.'){
-					tmp += (suffix_expression[j][k+1]-'0')*0.1;
-					break;
-				}
-				else tmp = suffix_expression[j][k]-'0'+tmp*10;
-			}
-			cout<<tmp<<endl;
-			s.push(tmp);
-		}	
-		else{
-			double t1,t2;
-			t1 = s.top();
-			s.pop();
-			t2 = s.top();
-			s.pop();
-			switch(suffix_expression[j][1]){
-				case '*':s.push(t2*t1);
-						break;
-				case '/':s.push(t2/t1);
-						break;
-				case '+':s.push(t2+t1);
-						break;
-				case '-':s.push(t2-t1);
-						break;
-			}
-		}
-		j++;
-	}
-	return s.top();
+double QuestionBank::calculate(string infix_expression) {
+    char suffix_expression[maxn][maxn];
+    infix_expression.append(1, '#');
+    change(infix_expression, suffix_expression);
+    stack<double> s;
+    int j = 0;
+    while (true) {
+        if (suffix_expression[j][0] == 'o' && suffix_expression[j][1] == '#')
+            break;
+        if (suffix_expression[j][0] == 'd') {
+            double tmp = 0;
+            for (int k = 1; suffix_expression[j][k]; k++) {
+                if (suffix_expression[j][k] == '.') {
+                    tmp += (suffix_expression[j][k + 1] - '0') * 0.1;
+                    break;
+                } else
+                    tmp = suffix_expression[j][k] - '0' + tmp * 10;
+            }
+            s.push(tmp);
+        } else {
+            double t1, t2;
+            t1 = s.top();
+            s.pop();
+            t2 = s.top();
+            s.pop();
+            switch (suffix_expression[j][1]) {
+            case '*':
+                s.push(t2 * t1);
+                break;
+            case '/':
+                s.push(t2 / t1);
+                break;
+            case '+':
+                s.push(t2 + t1);
+                break;
+            case '-':
+                s.push(t2 - t1);
+                break;
+            }
+        }
+        j++;
+    }
+    return s.top();
 }
